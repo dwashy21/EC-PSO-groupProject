ed9e2616eb3661ff69c6d617362e1bcb73cc1edd
Darius Washington <dc.wash95@gmail.com>
2017-11-24 18:02:59 -0600

    's' undefined in 'fitness_score()'

--- fitness.py
+++ fitness.py
@@ -4,7 +4,7 @@ from blackjack import *
 def fitness_score(strat,num_trials):
 	score=0
 	for i in range(0,num_trials):
-		score += play_one_game(s)
+		score += play_one_game(strat)
 	return score
 
 s = generate_strategy()
